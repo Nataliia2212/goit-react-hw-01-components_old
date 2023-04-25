48ffc729456edcaa8a63594e473d58678199952e
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2023-04-25 20:14:28 +0200

    aaa

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,17 +1,16 @@
 import PropTypes from 'prop-types';
 
-export const FriendListItem = ({ avatar, name, isOnline,id }) => {
+export const FriendListItem = ({ id, name, isOnline, avatar }) => {
     return <li className="item" key={id}>
-                <span className="status">{ isOnline}</span>
+                <span className="status">{isOnline}</span>
                 <img className="avatar" src={avatar} alt="avatar" width="48" />
                 <p className="name">{ name }</p>
             </li>
 }
 
-FriendListItem.PropTypes = {
+FriendListItem.propTypes = {
     id: PropTypes.number,
     name: PropTypes.string,
     isOnline: PropTypes.bool,
-    avatar: PropTypes.string
-    
+    avatar: PropTypes.string,
 }
